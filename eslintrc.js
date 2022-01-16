72e5b28a6a90905f5ea849fd7e5f15598ff6966e
MikeNats <michail.tsougkranis@gmail.com>
2022-01-16 12:01:46 +0200

    initial commit

---
+++ eslintrc.js
@@ -0,0 +1,18 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "prettier"
+    ],
+    "rules": {
+    }
+}
